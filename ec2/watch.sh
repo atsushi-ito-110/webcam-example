64a44cd28f887c8a471b6a4eb754806667dc450b
Atsushi Ito <atsushi.itoh66@gmail.com>
2022-05-15 14:39:34 +0000

    add: ec2/watch.sh

---
+++ ec2/watch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+WORK_DIR=/home/ec2-user/webcam-example
+/usr/bin/inotifywait -m -e create "$WORK_DIR/screenshots" | while read line; do
+  echo 'watch'
+  DATETIME=`date '+%Y%m%d-%H%M-%S'`
+  rm "$WORK_DIR/screenshots/picture.jpg"
+  echo "$DATETIME" >> ./watch.log
+  #echo $line | tee -a /var/log/dirmon.log
+  #dirmon_count=`cat /tmp/dirmon_count`
+  #echo `expr $dirmon_count + 1` > /tmp/dirmon_count
+done
